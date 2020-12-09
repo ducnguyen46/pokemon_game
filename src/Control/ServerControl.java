a6219a838c2ec58698751432d6a698ce7e24f02e
Nguyễn Ngọc Đức <nguyenngocduccl@gmail.com>
2020-12-09 13:00:01 +0700

    Dang ki

--- src/Control/ServerControl.java
+++ src/Control/ServerControl.java
@@ -17,6 +17,8 @@ import java.sql.Statement;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ServerControl {
 
@@ -64,7 +66,24 @@ public class ServerControl {
                 if (checkLogin(user)) {
                     oos.writeObject(user);
                 } else {
-                    oos.writeObject(new User(-1, null, null, null, -1, 1));
+                    oos.writeObject(new User(-1, null, null, null, -1, 0));
+                }
+            }
+            if(o instanceof String){
+                String request = (String)o;
+                
+                //dang ki
+                if(request.equalsIgnoreCase("!signUp")){
+                    Object signUpOject = ois.readObject();
+                    if(signUpOject instanceof User){
+                        User signUpUser = (User)signUpOject;
+                        boolean kq = signUp(signUpUser);
+                        if(kq){
+                            oos.writeObject(new String("SignUpOK"));
+                        } else {
+                            oos.writeObject(new String("SignUpNotOK"));
+                        }
+                    }
                 }
             }
         } catch (Exception e) {
@@ -72,7 +91,7 @@ public class ServerControl {
         }
     }
 
-    private boolean checkLogin(User user) throws Exception {
+    private boolean checkLogin(User user) {
         String sql = "SELECT * FROM user WHERE username = ? AND password = ?";
         try {
             PreparedStatement ps = con.prepareStatement(sql);
@@ -95,6 +114,32 @@ public class ServerControl {
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
+        }
+        return false;
+    }
+    
+    private boolean signUp(User user) {
+        String sqlCheck = "SELECT username FROM user WHERE username = ?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sqlCheck);
+            ps.setString(1, user.getUsername());
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()){
+                return false;
+            } else {
+                String sqlInsert = "INSERT INTO user VALUES(0, ?, ?, ?, 0);";
+                PreparedStatement psInsert = con.prepareStatement(sqlInsert, PreparedStatement.RETURN_GENERATED_KEYS);
+                psInsert.setString(1, user.getName());
+                psInsert.setString(2, user.getUsername());
+                psInsert.setString(3, user.getPassword());
+                
+                int kq = psInsert.executeUpdate();
+                if(kq != 0) return true;
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ServerControl.class.getName()).log(Level.SEVERE, null, ex);
         }
         return false;
     }
