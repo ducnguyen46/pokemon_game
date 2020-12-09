64c4bba3bce2c0da5f6a1e1dcca5be883672e4c9
Nguyễn Ngọc Đức <nguyenngocduccl@gmail.com>
2020-12-10 00:09:52 +0700

    hi

--- pikachu.sql
+++ pikachu.sql
@@ -24,11 +24,12 @@ CREATE TABLE game_detail (
     FOREIGN KEY (IDGame) REFERENCES game (ID),
     FOREIGN KEY (IDPlayer) REFERENCES user (ID)
 );
-
 INSERT INTO user VALUES
-('1', 'Đạt', 'deng126', '123456', 0, 0),
-('2', 'Đức', 'ducnguyen46', '654321', 0, 0);
+('1', 'Đạt', 'deng126', '123456', 0, 1),
+('2', 'Đức', 'ducnguyen46', '654321', 0, 1),
+('3', 'Sự', 'susmurf', '123456', 0, 1),
+('5', 'Minh', 'mnh', '111111', 0, 2);
 SELECT username, score, state FROM user WHERE state != 0 and username != 'deng126';
 SELECT username, score, state FROM user WHERE state != 0 AND username != ?;
-INSERT INTO user VALUES
-('5', 'Minh', 'mnh', '111111', 0, 2)
+-- INSERT INTO user VALUES
+-- ('5', 'Minh', 'mnh', '111111', 0, 2)
